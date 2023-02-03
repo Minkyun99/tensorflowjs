6e8c0c43ded6d3f187b56b3a0d50932d8cd6613d
Minkyun99 <kyung865821@gmail.com>
2023-02-03 17:48:55 +0900

    20230203

--- vue_tfjs/v01/public/crawling_학식정보.js
+++ vue_tfjs/v01/public/crawling_학식정보.js
@@ -2,26 +2,30 @@
 const axios = require("axios"); // 리퀘스트 용도
 const cheerio = require("cheerio"); // 선택자 용도
 
-const url =
-  "https://www.pusan.ac.kr/kor/CMS/MenuMgr/menuListOnBuilding.do?mCode=MN202";
+exports.ax = async function () {
+  let orst = "";
+  const url =
+    "https://www.pusan.ac.kr/kor/CMS/MenuMgr/menuListOnBuilding.do?mCode=MN202";
 
-axios.get(url).then((res) => {
-  let $ = cheerio.load(res.data);
-  let menu = [],
-    day = [],
-    date = [];
-  $(".day").each(function () {
-    day.push($(this).text());
-  });
-  $(".date").each(function () {
-    date.push($(this).text());
-  });
-  $(".menu-tit01~p").each(function () {
-    menu.push($(this).text());
-  });
+  await axios.get(url).then((res) => {
+    let $ = cheerio.load(res.data);
+    let menu = [],
+      day = [],
+      date = [];
+    $(".day").each(function () {
+      day.push($(this).text());
+    });
+    $(".date").each(function () {
+      date.push($(this).text());
+    });
+    $(".menu-tit01~p").each(function () {
+      menu.push($(this).text());
+    });
 
-  for (i in menu) {
-    const rst = `${date[i]}`;
-    orst += rst;
-  }
-});
+    for (i in menu) {
+      const rst = `${date[i]}`;
+      orst += rst;
+    }
+  });
+  return orst;
+};
--- vue_tfjs/v01/v01node.js
+++ vue_tfjs/v01/v01node.js
@@ -2,17 +2,25 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 const app = express();
+const history = require("connect-history-api-fallback");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const cw = require("./public/crawling_학식정보.js");
 const port = 3000;
 
 const _path = path.join(__dirname, "./dist");
 console.log(_path);
 
+app.use(history());
 app.use("/", express.static(_path));
 app.use(logger("tiny"));
+app.use(
+  createProxyMiddleware("/v1", {
+    target: "https//openapi.naver.com",
+    changeOrigin: true,
+  })
+);
 
 app.get("/hak", (req, res) => {
-  res.send("<Script>alert('반가워요')</Script>");
   cw.ax().then((v) => {
     console.log(v);
     res.send(v);
